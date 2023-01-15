a25e51efc63862a7d7136377dbba2fff9b329413
saquib <saquibzia07@gmail.com>
2023-01-16 01:23:00 +0530

    alignment of form, consoling form value and reset the value on clear btn click

--- src/components/Tokens.js
+++ src/components/Tokens.js
@@ -11,44 +11,63 @@ const [redTokens, setRedTokens] = useState(0);
 const [redPrefix, setRedPrefix] = useState('');
 const [redPerRow, setRedPerRow] = useState(0);
 
+
+// Creating function to capture/console values of the input field
   function generateTokens(e) {
-    console.log(e.target.values);
+    console.log(blueTokens);
+    console.log(bluePrefix);
+    console.log(bluePerRow);
+    console.log(redTokens);
+    console.log(redPrefix);
+    console.log(redPerRow);   
+  }
+
+// Creating function to reset the value of input field to initial state once clear button is clicked
+  function resetInputFields(){
+    setBlueTokens(0);
+    setBluePrefix('');
+    setBluePerRow(0);
+    setRedTokens(0);
+    setRedPrefix('');
+    setRedPerRow(0);
   }
 
   return (
     <div className="token-design">
         <h1> Token Generator Applicaion </h1>
-
+        
         <form>
-        Number of Blue Tokens: 
+
+        <label className='label-marg'>Number of Blue Tokens:</label> 
         <input type="number" value={blueTokens} onChange={e => setBlueTokens(e.target.value)} /> 
         <br/><br/>
 
-        Prefix for Blue Tokens:           
+        <label className='label-marg'>Prefix for Blue Tokens:</label>           
         <input type="text" value={bluePrefix} onChange={e => setBluePrefix(e.target.value)} /> 
         <br/><br/>
 
-        Blue Tokens per Row:
+        <label className='label-marg'>Blue Tokens per Row:</label>
         <input type="number" value={bluePerRow} onChange={e => setBluePerRow(e.target.value)} />
         <br/><br/>
 
-        Number of Red Tokens:
+        <label className='label-marg'>Number of Red Tokens:</label>
         <input type="number" value={redTokens} onChange={e => setRedTokens(e.target.value)} />
         <br/><br/>
 
-        Prefix for Red Tokens:
+        <label className='label-marg'>Prefix for Red Tokens:</label>
         <input type="text" value={redPrefix} onChange={e => setRedPrefix(e.target.value)} />
         <br/><br/>
 
-        Red Tokens per Row:
+        <label className='label-marg'>Red Tokens per Row:</label>
         <input type="number" value={redPerRow} onChange={e => setRedPerRow(e.target.value)} />
         <br/><br/>
 
         </form>
 
         <div className='tokenform-btns'>
-            <Button className='btn-sp' variant="contained" type='submit' onClick={generateTokens}>Generate</Button>
-            <Button variant="contained">Clear</Button>
+            <Button className='btn-sp' variant="contained" type='submit' 
+             onClick={generateTokens}>Generate</Button>
+            <Button variant="contained" type='reset' onClick={resetInputFields}>Clear</Button>
         </div>
      
     </div>
